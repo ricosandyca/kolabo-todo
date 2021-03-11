04fc656860d7a414c6f1a6042ba5ad7e6807dcb5
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-11 18:04:17 +0700

    Change color theme

--- client/src/config/theme.js
+++ client/src/config/theme.js
@@ -1,8 +1,13 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import blue from '@material-ui/core/colors/blue'
+import grey from '@material-ui/core/colors/grey'
 
 export default createMuiTheme({
   palette: {
+    background: {
+      default: '#ffffff',
+      paper: grey[50]
+    },
     primary: {
       main: blue[700],
       light: blue[500],
