1e70e3fe8190a5bdb65317780cea6506b9aa1639
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-11 20:58:46 +0700

    Change theme color scheme

--- client/src/config/theme.js
+++ client/src/config/theme.js
@@ -5,14 +5,23 @@ import grey from '@material-ui/core/colors/grey'
 export default createMuiTheme({
   palette: {
     background: {
-      default: '#ffffff',
+      default: '#fff',
       paper: grey[50]
     },
+    text: {
+      primary: grey[800]
+    },
     primary: {
+      main: grey[900],
+      light: grey[700],
+      dark: '#000',
+      contrastText: '#fff'
+    },
+    secondary: {
       main: blue[700],
       light: blue[500],
       dark: blue[900],
-      contrastText: '#ffffff'
+      contrastText: '#fff'
     }
   }
 })
