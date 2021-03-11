63095470243cbbad75d723dd744c761a3e2c0366
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-11 20:59:01 +0700

    Finish signin page

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,33 +1,120 @@
-import { signin } from '../services/auth/signin'
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import Box from '@material-ui/core/Box'
+import FormGroup from '@material-ui/core/FormGroup'
+import Typography from '@material-ui/core/Typography'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import EmailIcon from '@material-ui/icons/EmailOutlined'
+import PasswordIcon from '@material-ui/icons/VpnKeyOutlined'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+import CustomButton from '../components/CustomButton'
+import CustomInput from '../components/CustomInput'
+
 import useInput from '../hooks/useInput'
 import useAlert from '../hooks/useAlert'
+import { signin } from '../services/auth/signin'
 
-export default function SignInPage() {  
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    minHeight: '100vh'
+  },
+  formBox: {
+    width: '320px',
+    maxWidth: '320px',
+    padding: theme.spacing(3)
+  },
+  form: {
+    margin: `${theme.spacing(2)}px 0`,
+    '& > *': {
+      margin: `${theme.spacing(1)}px 0`
+    }
+  },
+  inputIcon: {
+    paddingRight: theme.spacing(.5),
+    paddingLeft: theme.spacing(.5),
+    color: theme.palette.grey[700]
+  }
+}))
+
+export default function SignInPage() {
+  const classes = useStyles()
+  const histroy = useHistory()
   const emailInput = useInput('')
   const passwordInput = useInput('')
-  const { Alert, setError, setSuccess, clearAlert } = useAlert()
+  const [isLoading, setIsLoading] = useState(false)
+  const { Alert, setError, clearAlert } = useAlert()
 
-  const handleSignin = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault()
+    setIsLoading(true)
     clearAlert()
-    const { value: email } = emailInput
-    const { value: password } = passwordInput
-    console.log(email, password)
     try {
-      const userCred = await signin(email, password)
-      console.log(userCred)
-      setSuccess('Success')
+      const { value: email } = emailInput
+      const { value: password } = passwordInput
+      await signin(email, password)
+      histroy.push('/dashboard')
     } catch (err) {
       setError(err.message)
     }
+    setIsLoading(false)
   }
 
   return (
-    <form onSubmit={handleSignin}>
-      <Alert />
-      <input type='email' {...emailInput} />
-      <input type='password' {...passwordInput} />
-      <input type='submit' value='submit' />
-    </form>
+    <Box className={classes.root} display='flex' justifyContent='center' alignItems='center'>
+      <Box className={classes.formBox}>
+        <Typography variant='h6' align='center'>Sign in to your account</Typography>
+        <form onSubmit={handleSubmit}>
+          <FormGroup className={classes.form}>
+
+            {/* Email input */}
+            <CustomInput
+              disabled={isLoading}
+              variant='outlined'
+              placeholder='Email'
+              type='email'
+              startAdornment={(
+                <InputAdornment position='start'>
+                  <EmailIcon />
+                </InputAdornment>
+              )}
+              {...emailInput}
+            />
+
+            {/* Password input */}
+            <CustomInput
+              disabled={isLoading}
+              variant='outlined'
+              placeholder='Password'
+              type='password'
+              startAdornment={(
+                <InputAdornment position='start'>
+                  <PasswordIcon />
+                </InputAdornment>
+              )}
+              {...passwordInput}
+            />
+
+            {/* Alert message */}
+            <Alert variant='filled' />
+
+            {/* Submit button */}
+            <CustomButton
+              variant='contained'
+              color='primary'
+              size='large'
+              disableElevation
+              type='submit'
+              disabled={isLoading}
+            >
+              {isLoading ? <CircularProgress size={24} /> : 'Submit'}
+            </CustomButton>
+
+          </FormGroup>
+        </form>
+      </Box>
+    </Box>
   )
 }
