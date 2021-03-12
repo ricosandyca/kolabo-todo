71ebc6367174bef42d5633166e4a2d083a199b6e
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-12 11:06:52 +0700

    Init recoil

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,13 @@
+import { RecoilRoot } from 'recoil'
 import withTheme from './hoc/withTheme'
 import IndexRoutes from './routes/IndexRoutes'
 
-export default withTheme(IndexRoutes)
+export default function App() {
+  const Themed = withTheme(IndexRoutes)
+
+  return (
+    <RecoilRoot>
+      <Themed />
+    </RecoilRoot>
+  )
+}
