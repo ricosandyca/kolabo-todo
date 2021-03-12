52dd39e2400a8942b2e5a257aa594b103efd3be0
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-12 17:49:57 +0700

    Add comment code

--- client/src/store/effects/persistentEffect.js
+++ client/src/store/effects/persistentEffect.js
@@ -1,3 +1,7 @@
+/**
+ * Persistent effect to store data locally through client local storage
+ * @param {String} localStorageKey - key of local storage
+ */
 export default function persistentEffect(localStorageKey) {
   return function ({ onSet, setSelf }) {
 
