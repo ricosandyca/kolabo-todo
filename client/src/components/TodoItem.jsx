f702495e3a1b502c2b8c942ff9a360a08aa987f2
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-12 09:25:31 +0700

    List todo of users

--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -1,5 +1,63 @@
-export default function TodoItem() {
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Tooltip from '@material-ui/core/Tooltip'
+import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/DeleteOutlineOutlined'
+import Typography from '@material-ui/core/Typography'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const useStyles = makeStyles(theme => ({
+  todoPaper: {
+    background: theme.palette.background.default,
+    padding: '5px',
+    marginBottom: theme.spacing(1.5)
+  },
+  doneTodo: {
+    background: theme.palette.background.paper
+  }
+}))
+
+export default function TodoItem({ todo }) {
+  const { body, done } = todo
+  const classes = useStyles()
+
   return (
-    <div>Todo Item</div>
+    <Paper
+      variant='outlined'
+      className={`${classes.todoPaper} ${done ? classes.doneTodo : ''}`}
+    >
+      <Box display='flex' flexDirection='row' justifyContent='space-between' alignItems='center'>
+
+        {/* Checkbox */}
+        <Box>
+          <Tooltip title='Check'>
+            <Checkbox
+              color='primary'
+              checked={done}
+              />
+          </Tooltip>
+        </Box>
+
+        {/* Todo body */}
+        <Box flex='1' paddingX='5px'>
+          <Typography
+            variant='body1'
+            style={{ textDecoration: done ? 'line-through' : 'none' }}
+          >
+            {body}
+          </Typography>
+        </Box>
+
+        {/* Delete button */}
+        <Box>
+          <Tooltip title='Delete'>
+            <IconButton color='secondary'>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Box>
+    </Paper>
   )
 }
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,5 +1,33 @@
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Box from '@material-ui/core/Box'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+import TodoItem from './TodoItem'
+import useAlert from '../hooks/useAlert'
+import { useGetAllTodo } from '../hooks/useTodo'
+import { useEffect } from 'react'
+
+const useStyles = makeStyles(theme => ({
+  notifBox: {
+    marginBottom: theme.spacing(1.5)
+  }
+}))
 export default function TodoList() {
+  const classes = useStyles()
+  const { todos, isLoading, error } = useGetAllTodo()
+  const { Alert, setError } = useAlert()
+
+  useEffect(() => {
+    if (error) setError(error.message)
+  }, [error, setError])
+
   return (
-    <div>Todo List</div>
+    <div>
+      {isLoading && <Box className={classes.notifBox}><LinearProgress color='primary' /></Box>}
+      {error && <Box className={classes.notifBox}><Alert /></Box>}
+      {todos?.map(todo => (
+        <TodoItem key={todo._id} todo={todo} />
+      ))}
+    </div>
   )
 }
